64f9dc8969be48449fe48fb4b48cab28df01a22a
sido200 <sidali20012017@outlook.com>
2026-02-22 13:57:15 +0100

    add traduction

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useTranslation } from "react-i18next"; // 1. Import du hook
+import { useTranslation } from "react-i18next"; 
 import {
   HiOutlineMenuAlt3,
   HiOutlineX,
